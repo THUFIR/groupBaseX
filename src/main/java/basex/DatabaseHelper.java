247cdd0c39cefcb0d866a0c5fa1e61be9f1942c7
thufir <hawat.thufir@gmail.com>
2020-02-15 04:56:28 -0800

    incremental

--- src/main/java/basex/DatabaseHelper.java
+++ src/main/java/basex/DatabaseHelper.java
@@ -1,19 +1,13 @@
 package basex;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.logging.Logger;
 import org.basex.core.BaseXException;
 import org.basex.core.Command;
 import org.basex.core.Context;
-import org.basex.core.cmd.CreateDB;
-import org.basex.core.cmd.DropDB;
 import org.basex.core.cmd.List;
 import org.basex.core.cmd.Open;
 import org.basex.core.cmd.Replace;
@@ -37,15 +31,6 @@ public class DatabaseHelper {
         this.properties = properties;
     }
 
-    private String wrap(String string) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-        stringBuilder.append("<root>");
-        stringBuilder.append(string);
-        stringBuilder.append("</root>");
-        return stringBuilder.toString();
-    }
-
     private void init() throws MalformedURLException, BaseXException {
         log.fine(properties.toString());
         parserType = properties.getProperty("parserType");
@@ -59,54 +44,23 @@ public class DatabaseHelper {
         log.fine(new List().execute(context));
     }
 
-    private void drop() throws BaseXException {
-        new Set("parser", parserType).execute(context);
-        new DropDB(databaseName).execute(context);
-        list();
-    }
+    private void replace() throws BaseXException, IOException {
 
-    private void create() throws BaseXException, JSONException {
-        new Set("parser", parserType).execute(context);
-        new CreateDB(databaseName).execute(context);
-        new List().execute(context);
-        list();
-    }
-
-    private void replace(JSONArray tweets) throws JSONException, BaseXException, IOException {
-        log.fine(tweets.toString());
-        JSONObject tweet = null;
-        long id = 0L;
         new Open(databaseName).execute(context);
-        new Set("parser", "json").execute(context);
         Command replace = null;
 
-        for (int i = 0; i < tweets.length(); i++) {
-            tweet = new JSONObject(tweets.get(i).toString());
-            id = Long.parseLong(tweet.get("id_str").toString());
-            replace = new Replace(id + ".xml");
-            replace.setInput(new ArrayInput(tweet.toString()));
+        for (int i = 1; i < 9; i++) {
             replace.execute(context);
         }
         log.fine((new XQuery(".")).execute(context).toString());
     }
 
-    public void dropCreateAdd(JSONArray tweets) throws MalformedURLException, BaseXException, JSONException, IOException {
+    public void dropCreateAdd() throws MalformedURLException, BaseXException, IOException {
         init();
         // drop();
         //  create();
-        replace(tweets);
+        replace();
         list();
     }
 
-    private void write(String tweet, String fileName) throws IOException {
-        log.fine(tweet);
-        FileWriter fileWriter = new FileWriter(fileName);
-        fileWriter.write(tweet);
-        fileWriter.close();
-    }
-
-    private String read(String fileName) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
-        return content;
-    }
 }
