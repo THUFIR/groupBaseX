dfc33850d824d8105a08a8107f86a5d067c09569
thufir <hawat.thufir@gmail.com>
2020-02-16 06:42:30 -0800

    incremental

---
+++ src/main/java/basex/FileHelper.java
@@ -0,0 +1,63 @@
+package basex;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+public class FileHelper {
+
+    private static final Logger log = Logger.getLogger(FileHelper.class.getName());
+    private Properties properties = null;
+    private List<String> lines = null;
+    private Person person = null;
+    private List<Person> people = new ArrayList<>();
+    private List<String> attributes = new ArrayList<>();
+
+    private FileHelper() {
+    }
+
+    public FileHelper(Properties properties) {
+        this.properties = properties;
+    }
+
+    public void readFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(properties.getProperty("dataFile")));
+        lines = new ArrayList();
+        reader.lines().forEach(line -> lines.add(line));
+        Collections.reverse(lines);
+        log.fine(lines.toString());
+    }
+
+    public void processLines() {
+        String regex = "\\D+";
+        boolean isDigit = false;
+        for (String s : lines) {
+            isDigit = Pattern.matches(regex, s);
+            log.fine(s + "\t\t" + Boolean.toString(isDigit));
+            if (!isDigit) {
+                addAttribute(s);
+            } else {
+                addPerson(s);
+            }
+        }
+        log.info(people.toString());
+    }
+
+    private void addAttribute(String s) {
+        attributes.add(s);
+    }
+
+    private void addPerson(String name) {
+        person = new Person(name, attributes);
+        people.add(person);
+        person = null;
+        attributes = new ArrayList<>();
+    }
+
+}
---
+++ src/main/java/basex/Person.java
@@ -0,0 +1,30 @@
+package basex;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Person {
+
+    private static final Logger log = Logger.getLogger(Person.class.getName());
+    private String name = "";
+    private List<String> attributes = new ArrayList<>();
+
+    private Person() {
+    }
+
+    Person(String name, List<String> attributes) {
+        this.name = name;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : attributes) {
+            stringBuilder.append(s);
+            stringBuilder.append("\t");
+        }
+        return "\n\n\n" + name + "\n" + stringBuilder.toString();
+    }
+}
