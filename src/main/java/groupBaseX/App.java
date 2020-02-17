b3200fd516dec806002955bdaa1239d8362320c9
thufir <hawat.thufir@gmail.com>
2020-02-17 02:25:55 -0800

    incremental

--- src/main/java/groupBaseX/App.java
+++ src/main/java/groupBaseX/App.java
@@ -1,15 +1,10 @@
 package groupBaseX;
 
-import basex.DatabaseHelper;
-import basex.FileHelper;
-import basex.Person;
 import basex.Reader;
 import java.io.IOException;
-import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
 import org.basex.core.BaseXException;
-import org.json.JSONArray;
 
 public class App {
 
@@ -17,21 +12,9 @@ public class App {
     private Properties properties = new Properties();
 
     private void run() throws BaseXException, IOException   {
-        properties.loadFromXML(App.class.getResourceAsStream("/real.xml"));
+        properties.loadFromXML(App.class.getResourceAsStream("/properties.xml"));
         basex.Reader dq = new Reader(properties);
         dq.iterate();
-    }
-
-    private void load() throws IOException {
-        properties.loadFromXML(App.class.getResourceAsStream("/real.xml"));
-        log.fine(properties.toString());
-        FileHelper f = new FileHelper(properties);
-        List<Person> people = f.getPeople();
-        log.fine(people.toString());
-        DatabaseHelper db = new DatabaseHelper(properties);
-        JSONArray jsonPeople = db.addPeople(people);
-        log.info(jsonPeople.toString());
-        //   db.persist(jsonPeople);
     }
 
     public static void main(String[] args) throws IOException {
